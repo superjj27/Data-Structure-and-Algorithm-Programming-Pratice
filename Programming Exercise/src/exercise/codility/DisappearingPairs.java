b79f0a0893756d6b7e57e63a310a0ffe07f50042
Hung Chun Ho Jeffrey <skyjeff20@gmail.com>
2023-08-29 20:19:59 +1000

    add one more solved question "DisappearingPairs" from Codility

---
+++ Programming Exercise/src/exercise/codility/DisappearingPairs.java
@@ -0,0 +1,31 @@
+package exercise.codility;
+
+public class DisappearingPairs {
+
+    private static final String[] pairs = {"AA", "BB", "CC"};
+    public static String findDisappearingPairs(String S) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(S);
+
+        for (int i=0; i<pairs.length;) {
+            String temp = sb.toString().replaceAll(pairs[i], "");
+            if (temp.length() < sb.length()) {
+                sb = new StringBuilder().append(temp);
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(final String[] args) {
+        // ABABAAB
+        // ABABB
+        // ABABB
+        // ABA
+        String S = "ABABACCAB";
+        System.out.println(findDisappearingPairs(S));
+    }
+}
